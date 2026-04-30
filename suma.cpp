d39a9452ca5b4eb3d333c1c904cf3e54a45fd952
elvixlus977-byte <elvixlus977@gmail.com>
2026-04-30 03:47:56 +0000

    agregado suma.cpp

---
+++ suma.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int a,b;
+  cout<<"ingrese un numero "<<endl;
+  cin>>a;
+  cout<<"ingrese otro numero "<<endl;
+  cin>>b;
+  return 0;
+}

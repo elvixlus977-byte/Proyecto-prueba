0df0d3b8176e41f9f1485b9616e0dff407485d39
lunex897-blip <lunex897@gmail.com>
2026-04-30 18:43:14 +0000

    este lo hizo lunex

---
+++ suma de 4 .cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std; 
+
+int main(){
+    int num, suma = 0;
+    
+    for (int i = 1; i <= 4; i++){
+        cout << "Ingrese el numero " << i << ": ";
+        cin >> num;
+        suma += num;
+    }
+    cout<<"El valor de la suma de los 4 valores es "<<suma<<endl;
+}

4f12b2eca8fe1cfb0fd1b881c381cbc8a468ffd3
elvixlus977-byte <elvixlus977@gmail.com>
2026-05-01 10:51:06 -0600

    esto es una suam de 4 pero subido desde la computadora

---
+++ suma4.2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, suma = 0;
+
+    for (int i = 1; i <= 4; i++) {
+        cout << "Ingrese el numero " << i << ": ";
+        cin >> num;
+        suma += num;
+    }
+
+    cout << "El valor de la suma de los 4 valores es " << suma << endl;
+
+     for (int i = 1; i <= 10; i++) {
+        cout << suma << " * " << i << " = " << suma * i << endl;
+    }
+
+return 0;
+}
